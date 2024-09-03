5ae34bb22a97510a770bdc6833595a6e320ab0fb
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2024-09-03 01:47:02 +0100

    http service added for getting path

---
+++ lib/services/http_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+
+class HTTPService {
+  HTTPService();
+
+  final _dio = Dio();
+
+  Future<Response?> get(String path) async {
+    try {
+      Response res = await _dio.get(path);
+      return res;
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+}
